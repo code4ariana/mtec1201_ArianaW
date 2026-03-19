dfa8ca9e255c5fc72529a5b289baef68ec2a11f1
ArianaWalcott <arianawalcottgd@gmail.com>
2026-03-19 13:19:27 -0400

    Short Study 1 Assignment

---
+++ ShortStudy1/The_Pizza_-_Short_Study_1_2026_03_19_17_18_25/sketch.js
@@ -0,0 +1,65 @@
+function setup() {
+  createCanvas(1000, 500);
+  //A canvas width of at least 500 pixels, max 1920 pixels. yes;
+  // A canvas height of at least 500 pixels, max 1080 pixels. yes;
+}
+
+// Ariana W.
+
+// I decided to sketch a pizza because my study will explore how communities form around interactive media, such as video games, through shared food experiences. Pizza is a unifying food often seen in games geared toward young people, making it an ideal subject. Two great examples are Papa’s Pizzeria and Work at a Pizza Place.
+
+// This sketch is a pizza wi
+
+function draw() {
+  background(220);
+  
+  
+  
+  strokeWeight(5);
+  // crust color
+  //stroke(140, 38, 23);
+  
+  
+  strokeWeight(2);
+  // pie crust
+  fill(140, 38, 0);
+  circle(250,250, 300);
+  
+    
+ 
+  // cheese
+  fill(249,208,46);
+  circle (250, 250, 240);
+  
+  // olives
+  fill("black");
+  ellipse(300, 200, 20, 50, );
+  
+  
+  // green onion (1)
+ fill("green");
+rect(220, 260, 90, 10);
+  
+  
+  // green onion (2)
+ // strokeWeight(20);
+ fill("green");
+ rect(180, 300, 90, 10);
+  
+  // peppronni 1
+  fill("red");
+  circle(200,200, 40);
+
+  
+  // peppronni 2
+  fill("red");
+  circle(300,275, 40);
+  
+   // peppronni 3
+  strokeWeight(1);
+  fill("red");
+  circle(200,287, 30);
+ 
+  
+  
+}
